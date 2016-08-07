df200df804ea1ef6dec98b134788a6d159edff66
flubadoo <sen.huang@yale.edu>
2016-08-07 16:58:31 +0800

    now fetches the url

--- dontforget/sample.js
+++ dontforget/sample.js
@@ -1,10 +1,13 @@
 chrome.runtime.onInstalled.addListener(function() {
   //not sure why this has to be a list but oh well
-  var contexts = ["selection"];
-  for (var i = 0; i < contexts.length; i++) {
-    var context = contexts[i];
-    var title = "Remember this quote"
-    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "id": "context" + context});
-  }
+  var context = "selection";
+  var title = "Remember this quote";
+  var id = chrome.contextMenus.create({"title":title, "contexts":[context], "id":"context"+context})
 });
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+function onClickHandler(info, tab) {
+  var tab_url = tab && tab.url;
+  window.open(tab_url)
+};
